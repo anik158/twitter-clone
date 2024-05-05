5021a93418c5e2dc53e803b44e442d218ad90973
wanderer <wanderer@localhost.localdomain>
2024-05-06 02:03:59 +0600

    creating layout for blade

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <h2>Terms</h2>
+    <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, repellendus sit? Aliquam, aperiam labore molestiae optio perspiciatis quae veritatis voluptate. Asperiores beatae earum eius eligendi facilis laboriosam praesentium quae. Corporis deleniti dignissimos eum ex excepturi exercitationem explicabo iste nemo omnis, quaerat quia quo suscipit veritatis. Aliquid animi aperiam autem consequatur cupiditate doloremque dolores eius harum illum iusto maxime, neque nobis, non nostrum nulla numquam officia omnis pariatur provident qui quia quidem ratione repellat repudiandae similique vel voluptatibus voluptatum! Dolor, voluptates?
+    </div>
+@endsection
+
